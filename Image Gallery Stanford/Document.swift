39d0e3b2b4c192785d966ef9d580ff458f15f723
Sevenfortyseven <alekokhubuluri1994@gmail.com>
2022-07-18 12:54:45 +0400

    Initial commit

---
+++ Image Gallery Stanford/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Image Gallery Stanford
+//
+//  Created by aleksandre Khubuluri on 18.07.22.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
